cfba648bceb2b3243c3f8c990edf35f493d779a7
Aziz Can Öz <oz.azizcann@gmail.com>
2024-08-05 19:54:29 +0300

    init

---
+++ src/main/java/com/test/cryptoPriceTrackingAndPortfolio/CryptoPriceTrackingAndPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.test.cryptoPriceTrackingAndPortfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoPriceTrackingAndPortfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoPriceTrackingAndPortfolioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/cryptoPriceTrackingAndPortfolio/CryptoPriceTrackingAndPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.cryptoPriceTrackingAndPortfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoPriceTrackingAndPortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
