94d8056b044dbd0230628dacbec78762212c5194
Aziz Can Öz <oz.azizcann@gmail.com>
2024-08-11 15:11:20 +0300

    async enabled for mail service

--- src/main/java/com/test/cryptoPriceTrackingAndPortfolio/CryptoPriceTrackingAndPortfolioApplication.java
+++ src/main/java/com/test/cryptoPriceTrackingAndPortfolio/CryptoPriceTrackingAndPortfolioApplication.java
@@ -7,8 +7,10 @@ import org.modelmapper.PropertyMap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@EnableAsync
 @SpringBootApplication
 public class CryptoPriceTrackingAndPortfolioApplication {
 
